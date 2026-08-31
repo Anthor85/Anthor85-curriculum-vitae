b80d0563b300e2f9ebfd5db1e8127d4e73d9f835
Anthor85 <ammlink@hotmail.com>
2026-08-31 11:48:27 +0200

    Edit formacion backend

--- amm-curriculum-vitae-backend/routes/formacion.js
+++ amm-curriculum-vitae-backend/routes/formacion.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const {
   obtenerFormaciones,
   crearFormacion,
+  actualizarFormacion,
   deleteFormacion,
 } = require('../controllers/formacion');
 
@@ -9,6 +10,7 @@ const router = Router();
 
 router.get('/', obtenerFormaciones);
 router.post('/', crearFormacion);
+router.put('/:id', actualizarFormacion);
 router.delete('/:id', deleteFormacion);
 
 module.exports = router;
